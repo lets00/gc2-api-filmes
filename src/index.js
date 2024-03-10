b6b8e434ad052ab0f14c6af54dab9b8717662ed5
Luís Eduardo <leduardo@lsd.ufcg.edu.br>
2024-03-10 10:47:16 -0300

    Revert "Feature/get all movies endpoint"

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import app from './app.js';
-
-app.listen(8000, () => {
-  console.log('Running server at port:', 8000);
-});
--- src/routes/movie.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { findAllMovies } from '../controllers/movieController.js'
-
-const router = express.Router()
-
-router.get('/', findAllMovies)
-
-export default router
